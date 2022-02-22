1003732270132b7fe7d7b0ddd47dec4edf59ff82
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2022-02-22 20:04:19 -0300

    ADD aula02

--- aula02/aula/main.js
+++ aula02/aula/main.js
@@ -19,3 +19,9 @@ for( let detail in user){
 //exemplo prompt enviando a mensagem para o console
 
 console.log(prompt());
+
+//pega uma string e transforma em número
+parseInt();
+
+//um numero randômico entre 1 e 3
+Math.random()*1+3
