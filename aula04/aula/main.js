65f66c91144fcf6f3ab5bb74b30ccacd158416b7
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2022-03-07 20:52:31 -0300

    ADD aula04 conteúdo e mesa de trabalho

---
+++ aula04/aula/main.js
@@ -0,0 +1,17 @@
+//Pega pela tag
+document.getElementsByTagName();
+
+//Pega pelo id
+document.getElementById();//
+
+//Pega pela classe
+document.getElementsByClassName();
+
+//Pega o PRIMEIRO elemento de qualquer coisa: tag (nomeDaTag), id (#nomeDoId), classe(.nomeDaClasse)
+document.querySelector();//
+
+//Pega o TODOS elementos de qualquer coisa: tag (nomeDaTag), id (#nomeDoId), classe(.nomeDaClasse)
+document.querySelectorAll();
+
+//Muda o CSS
+document.getElementById("vermelho").style.color = 'red'
