b3b58e73274c84c892a63b28c2b058e58d1d25b2
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2022-03-22 19:18:07 -0300

    Pequenas correções acentuação/pontuação

--- aula10/mesa/main.js
+++ aula10/mesa/main.js
@@ -17,13 +17,13 @@ submitButtonReference.addEventListener('click',function(event){
     let fullName = userName.concat(' ').concat(userSurname)
     let noAnoE = fullName.replace(/a/g,"@").replace(/e/g,"3")
 
-    outputDataReference.innerHTML = `
+    outputDataReference.innerHTML += `
     <div>
-    <p>O nome do usuário sem espaços extras: ${userName.trim()}.</p>
-    <p>O nome do usuário com todas as letras maiúsculas: ${userName.toUpperCase()}.</p>
-    <p>O nome do usuário com todas as letras minúsculas: ${userName.toLowerCase()}.</p>
-    <p>O nome completo do usuário: ${fullName}.</p>
-    <p>Subtituindo o "a" por "@" e o "e" por "3": ${noAnoE} </p>
-</div>  
+        <p>O nome do usuário sem espaços extras: ${userName.trim()}.</p>
+        <p>O nome do usuário com todas as letras maiúsculas: ${userName.toUpperCase()}.</p>
+        <p>O nome do usuário com todas as letras minúsculas: ${userName.toLowerCase()}.</p>
+        <p>O nome completo do usuário: ${fullName}.</p>
+        <p>Subtituindo o "a" por "@" e o "e" por "3": ${noAnoE}. </p>
+    </div>  
     `
 })
